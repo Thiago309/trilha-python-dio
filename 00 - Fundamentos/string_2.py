56e509a60b06ff2350cba29e4154aeeae242b6be
Thiago Vinicius <116599158+Thiago309@users.noreply.github.com>
2025-07-25 12:32:17 -0300

    Update string_2.py

--- 00 - Fundamentos/string_2.py
+++ 00 - Fundamentos/string_2.py
@@ -4,7 +4,7 @@ profissao = "Progamador"
 linguagem = "Python"
 saldo = 45.435
 
-dados = {"nome": "Guilherme", "idade": 28}
+dados = {"nome": "Guilherme", "idade": 28, "sddicionario": 67.43}
 
 print("Nome: %s Idade: %d" % (nome, idade))
 
@@ -15,7 +15,7 @@ print("Nome: {1} Idade: {0} Nome: {1} {1}".format(idade, nome))
 
 print("Nome: {nome} Idade: {idade}".format(nome=nome, idade=idade))
 print("Nome: {name} Idade: {age} {name} {name} {age}".format(age=idade, name=nome))
-print("Nome: {nome} Idade: {idade}".format(**dados))
+print("Nome: {nome} Idade: {idade} Saldo {sddicionario}".format(**dados))
 
 print(f"Nome: {nome} Idade: {idade}")
 print(f"Nome: {nome} Idade: {idade} Saldo: {saldo:.2f}")
